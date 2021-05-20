f71a66756983cf055712cfc2f5cebc4c3b96b08e
Charlie Traylor <ctraylor@tql.com>
2021-05-20 16:32:57 -0400

    quad done

---
+++ Geometric Shapes Solution/Geometric Shapes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Geometric_Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q1 = new Quad();
+            q1.Side1 = 2;
+            q1.Side2 = 3;
+            q1.Side3 = 4;
+            q1.Side4 = 5;
+
+            Console.WriteLine($"The perimeter of the Quadrilateral is {q1.Perimeter()}");
+
+           
+
+        }
+    }
+}
---
+++ Geometric Shapes Solution/Geometric Shapes/Quad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometric_Shapes
+{
+    class Quad
+    {
+       
+       
+
+        public int Side1 { get; set; }
+        public int Side2 { get; set; }
+        public int Side3 { get; set; }
+        public int Side4 { get; set; }
+
+        public int Perimeter()
+        {
+            return Side1 + Side2 + Side3 + Side4;
+        }
+
+    }
+}
