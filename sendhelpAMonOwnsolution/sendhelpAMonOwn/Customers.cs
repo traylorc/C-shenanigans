ef7752c252c5470d434e33f9c5195f69b85f4cfc
Charlie Traylor <ctraylor@tql.com>
2021-05-20 13:35:06 -0400

    customer add ins

---
+++ sendhelpAMonOwnsolution/sendhelpAMonOwn/Customers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sendhelpAMonOwn
+{
+    class Customers
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public decimal Sales { get; private set; }
+        public bool Active { get; set; } = true;
+        private static int LastId { get; set; } = 0;
+
+        public void AddSales(decimal amount)
+        {
+            Sales = Sales + amount;            
+        }
+
+
+       public Customers (string name, string city, string state)
+        {
+            this.Id = LastId;
+            LastId++;
+            this.Name = name;
+            this.City = city;
+            this.State = state;
+            
+        }
+    }
+}
---
+++ sendhelpAMonOwnsolution/sendhelpAMonOwn/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace sendhelpAMonOwn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new Customers("Amazon", "Stockton", "CA");
+            var c2 = new Customers("Staples", "Tracy", "CA");
+            var c3 = new Customers("GameStop", "Hayward", "CA");
+
+            c1.AddSales(1000);
+            c1.AddSales(3800);
+            Console.WriteLine($"Customer {c1.Name} sales is {c1.Sales}");
+        }
+    }
+}
