6b8258c49eb06e8cab5b9624722c9b0b6ddba102
Charlie Traylor <ctraylor@tql.com>
2021-05-21 15:41:28 -0400

    savingscomposition

---
+++ Bankingsolution/Banking/SavingsComposition.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking
+{
+    class SavingsComposition
+    {
+        private Account account = new Account();
+
+
+        public bool Deposit(decimal amount)
+        {
+            return account.Deposit(amount);
+        }
+
+
+        public bool Withdrawal(decimal amount)
+        {
+            return account.Withdrawal(amount);
+        }
+        
+        public bool Transfer(decimal amount, Account toAccount)
+        {
+            return account.Transfer(amount, toAccount);
+        }
+        public SavingsComposition()
+        {
+            account = new Account();
+        }
+
+
+
+
+
+        public decimal InterestRate { get; private set; } = 0.12m;
+
+        public decimal CalculateInterestByMonths(int NumberOfMonths)
+        {
+            return account.Balance * (InterestRate / 12.0m) * NumberOfMonths;
+        }
+
+        public void PayInterest(int NumberOfMonths)
+        {
+            var interest = CalculateInterestByMonths(NumberOfMonths);
+            Deposit(interest);
+        }
+
+    }
+}
