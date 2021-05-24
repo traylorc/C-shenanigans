7cd926d90bcb6b37ef06053ea089b6b608651541
Charlie Traylor <ctraylor@tql.com>
2021-05-24 12:30:19 -0400

    exception example

---
+++ ExceptionLessonSolution/ExceptionLesson/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExceptionLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Level1();
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine($"Exception handled in Level1: {ex.Message}");
+            }
+        }
+
+        static void Level1()
+        {
+            Level2();
+        }
+        static void Level2()
+        {
+            Level3();
+        }
+        static void Level3()
+        {
+            var n = 1;
+            var d = 0;
+            var e = n / d;
+        }
+        
+       
+
+    }
+}
