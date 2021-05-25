3fed6609e7db2d92f4528819b510fc46d31264ed
Charlie Traylor <ctraylor@tql.com>
2021-05-25 12:12:12 -0400

    libraryexample

---
+++ LobraryLessonSolution/LibraryTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using LibraryLesson;
+using System;
+
+namespace LibraryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //remember static lets us do the below without creating and instance for 'Mathlib', we can directly reference it
+            var oneplustwo = MathLib.Add(1, 2);
+            var sevenminustwo = MathLib.Sub(7, 2);
+            var twoxtwo = MathLib.Product(2, 2);
+            var tendividetwo = MathLib.Divide(10, 2);
+            var twelvemodulothree = MathLib.Modulo(12, 3);
+        }
+    }
+}
---
+++ LobraryLessonSolution/LobraryLesson/MathLib.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LibraryLesson
+{
+    public class MathLib
+    {
+        //create method and return result
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Product(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+       
+        public static int Modulo(int a, int b)
+        {
+            return a - (Product(Divide(a,b), b));
+        }
+    }
+}
