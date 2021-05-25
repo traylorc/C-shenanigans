92fca36c1cb278752ca8519e49eb912b797e2bb0
Charlie Traylor <ctraylor@tql.com>
2021-05-25 10:29:10 -0400

    fibanucii

---
+++ Fib sequence solution/Fib sequence/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Fib_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // connect in the method from fibcreator into method
+            List<int> fibs = fibcreator.GenerateSequence(200);
+            //set initial value for the sum of the numbers
+            var sum = 0;
+            // for each number(fib) in the list of numbers(fibs)
+            foreach(var fib in fibs)
+            {
+                //add number fib to previous sum to get new sum
+                sum = sum + fib;
+            }
+            //average is the sum of the numbers divided by the quantity of numbers in the list
+            var avg = sum / fibs.Count;
+            Console.WriteLine($"Fibs avg is {avg}");
+        
+
+        }
+    }
+}
---
+++ Fib sequence solution/Fib sequence/fibcreator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fib_sequence
+{
+    public class fibcreator
+    {
+        //start method, method static can only access other static methods, best use is when you dont have any properties to pass in
+        public static List <int> GenerateSequence(int last = 200)
+        {
+            //create a var name for your list and add parens for a value
+            var fibs = new List<int>();
+            //create var 'a' and add it to the list
+            var a = 0;
+            fibs.Add(a);
+            //creat var 'b' and add it to the list
+            var b = 1;
+            fibs.Add(b);
+            //set initial value for 'c'
+            int c = 0;
+            //while 'c' is lower than the last number in the sequence(200)
+            while(c< last)
+            {
+                // 'c' equals 'a' + 'b', add the new 'c' to the list
+                c = a + b;
+                fibs.Add(c);
+                //old 'b' becomes new 'a', 'c' becomes new 'b', rinse and repeat till reaches(200)
+                a = b;
+                b = c;
+            }
+            // return list
+            return fibs;
+        }
+    }
+}
