e48a6a6b626599a6bbc0fbcd36d5e362a0e98676
Charlie Traylor <ctraylor@tql.com>
2021-05-25 12:33:45 -0400

    Extension example

---
+++ EtensionMethodsLessonSolution/EtensionMethodsLesson/MyExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EtensionMethodsLesson
+{
+    static class MyExtensionMethods
+    {
+        //this var type and var name
+        public static string ToUpperCase(this string s)
+        {
+            return s.ToUpper();
+        }
+        public static void ToConsole(this string str)
+        {
+            Console.WriteLine($"{str}");
+        }
+    }
+}
---
+++ EtensionMethodsLessonSolution/EtensionMethodsLesson/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EtensionMethodsLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //call to ToConsole class extension
+            var abc = "Sorry for the delay, we are trying so hard but are not equipped for this pressure";
+            abc.ToUpper().ToConsole();
+        }
+    }
+}
