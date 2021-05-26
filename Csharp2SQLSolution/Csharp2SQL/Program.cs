54d9f780abd7868e648a494bd1d9847717f1f0cc
Charlie Traylor <ctraylor@tql.com>
2021-05-26 12:30:01 -0400

    added insert update delete sql statements

--- Csharp2SQLSolution/Csharp2SQL/Program.cs
+++ Csharp2SQLSolution/Csharp2SQL/Program.cs
@@ -7,18 +7,36 @@ namespace Csharp2SQL
     {
         static void Main(string[] args)
         {
-
-
-             //create var name and call to method
+             //create var name and stuff in desired class/project and call to method using new var//
             var sqllib = new SQLLib();
             sqllib.Connect();
 
 
+            //var user = sqllib.GetByPK(6);
+            //var success = sqllib.Remove(user);
 
 
-            var users = sqllib.GetAllUsers();
-            var user = sqllib.GetByPK(1);
-            var vndrs = sqllib.GetAllVendors();
+            //var user = sqllib.GetByPK(6);
+            //user.Phone = "3334445678";
+            //var success = sqllib.Change(user);
+
+
+
+            //set var for the new user//
+            //var newuser = new User()
+            //{
+            //    //add in values for required fields
+            //    Id = 0, Username = "Barkboi1", Password = "Aarf", Firstname = "Dog", Lastname = "Boi",
+            //    Phone = "888", Email = "yeet", IsReviewer = true, IsAdmin = true 
+            //};
+            // create var for data to feed into, call desired method //
+            //var success = sqllib.Create(newuser);
+
+
+
+
+            //var users = sqllib.GetAllUsers();
+            //var vndrs = sqllib.GetAllVendors();
 
 
 
--- Csharp2SQLSolution/Csharp2SQLLibrary/SQLLib.cs
+++ Csharp2SQLSolution/Csharp2SQLLibrary/SQLLib.cs
@@ -9,6 +9,91 @@ namespace Csharp2SQLLibrary
         //set property for connection
         public SqlConnection sqlconn { get; set; }
 
+
+        public bool Remove(User user)
+        {
+            // create var to stuff sql statment into
+            var sql = $" Delete from Users " +                        
+                        " Where Id = @id; ";
+
+            var sqlcmd = new SqlCommand(sql, sqlconn);           
+            sqlcmd.Parameters.AddWithValue("@id", user.Id);
+            var rowsaffected = sqlcmd.ExecuteNonQuery();
+
+            return (rowsaffected == 1);
+        }
+
+
+
+        //create user instance
+        public bool Change(User user)
+        {
+            // create var to stuff sql statment into
+            var sql = $"UPDATE Users Set " +
+                        " Username = @username, " +
+                        " Password = @password, " +
+                        " Firstname = @firstname, " +
+                        " Lastname = @lastname, " +
+                        " Phone = @phone, " +
+                        " Email = @email, " +
+                        " IsReviewer = @isreviewer " +
+                        " IsAdmin = @isadmin " +
+                        " Where Id = @id; ";
+
+            var sqlcmd = new SqlCommand(sql, sqlconn);
+
+            sqlcmd.Parameters.AddWithValue("@username", user.Username);
+            sqlcmd.Parameters.AddWithValue("@password", user.Password);
+            sqlcmd.Parameters.AddWithValue("@firstname", user.Firstname);
+            sqlcmd.Parameters.AddWithValue("@lastname", user.Lastname);
+            sqlcmd.Parameters.AddWithValue("@phone", user.Phone);
+            sqlcmd.Parameters.AddWithValue("@email", user.Email);
+            sqlcmd.Parameters.AddWithValue("@isreviewer", user.IsReviewer);
+            sqlcmd.Parameters.AddWithValue("@isadmin", user.IsAdmin);
+            sqlcmd.Parameters.AddWithValue("@id", user.Id);
+            var rowsaffected = sqlcmd.ExecuteNonQuery();
+
+            return (rowsaffected == 1);
+        }
+
+
+        // to create multiple users at once
+        public bool CreateMultiple(List<User> users)
+        {
+            var success = true;
+            foreach(var user in users)
+            {
+                //if create works, create user is true. success is already true. true and true = true, 
+                success = success && Create(user);
+            }
+            return true;
+        }
+
+
+        // boolean statement to say did this work or not, to create single user
+        public bool Create(User user)
+        {
+            var sql = $"INSERT into Users" +
+                "(Username, Password, Firstname, Lastname, Phone, Email, IsReviewer, IsAdmin)" +
+                " Values " +
+               $" (@username, @password, @firstname, @lastname, " +
+               $" @phone, @email, @isreviewer, @isadmin); ";
+            var sqlcmd = new SqlCommand(sql, sqlconn);
+
+            sqlcmd.Parameters.AddWithValue("@username", user.Username);
+            sqlcmd.Parameters.AddWithValue("@password", user.Password);
+            sqlcmd.Parameters.AddWithValue("@firstname", user.Firstname);
+            sqlcmd.Parameters.AddWithValue("@lastname", user.Lastname);
+            sqlcmd.Parameters.AddWithValue("@phone", user.Phone);
+            sqlcmd.Parameters.AddWithValue("@email", user.Email);
+            sqlcmd.Parameters.AddWithValue("@isreviewer", user.IsReviewer);
+            sqlcmd.Parameters.AddWithValue("@isadmin", user.IsAdmin);
+            var rowsaffected = sqlcmd.ExecuteNonQuery();
+           
+            return (rowsaffected == 1);
+        }
+
+
         public List<Vendors> GetAllVendors()
         {
             //add in SQL Statement
@@ -56,34 +141,21 @@ namespace Csharp2SQLLibrary
 
 
         }
+        
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
         public User GetByPK(int id)
         {
             var sql = $"SELECT * from users where id ={id};";
             var sqlcmd = new SqlCommand(sql, sqlconn);
             var sqldatareader = sqlcmd.ExecuteReader();
-            
+            // .hasrows shows how many rows in selected column
             if(!sqldatareader.HasRows)
             {
                 sqldatareader.Close();
                 return null;
             }
+            // .read starts before data, moves through data to process, ends right after data
             sqldatareader.Read();
             var user = new User()
             {
@@ -109,7 +181,7 @@ namespace Csharp2SQLLibrary
         {
             //add in SQL Statement
             var sql = "SELECT * From Users;";
-            //set new instance of SqlCommand
+            //set new instance of SqlCommand, built in method
             var sqlcmd = new SqlCommand(sql, sqlconn);
             //reader brings back results so you can read them
             var sqldatareader = sqlcmd.ExecuteReader();
@@ -133,6 +205,7 @@ namespace Csharp2SQLLibrary
                 //create instance of user class
                 var user = new User()
                 {
+                    // big to small
                     Id = id, Username = username, Password = password,
                     Firstname = firstname, Lastname = lastname, Phone = phone,
                     Email = email, IsAdmin = isAdmin, IsReviewer = isReviewer
@@ -143,6 +216,9 @@ namespace Csharp2SQLLibrary
             return users;
         }
 
+
+
+
         //create connect method
         public void Connect()
         {
@@ -150,7 +226,9 @@ namespace Csharp2SQLLibrary
                 "database=PrsDb;" +
                 "trusted_connection=true;";
             sqlconn = new SqlConnection(connStr);
+            //run open method to open the connection 
             sqlconn.Open();
+            //use if statement below to test connection
             if(sqlconn.State != System.Data.ConnectionState.Open)
             {
                 throw new Exception("Connection string is not correct");
@@ -159,7 +237,7 @@ namespace Csharp2SQLLibrary
             Console.WriteLine("Open connection Successfull");
         }
 
-        //create disconnect method
+        //create disconnect method, always close connection when done
         public void Disconnect()
         {
             if (sqlconn == null)
