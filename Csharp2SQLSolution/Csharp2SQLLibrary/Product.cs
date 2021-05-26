c477c42baa55ac074336f77c3b6c27670f3a6eb0
Charlie Traylor <ctraylor@tql.com>
2021-05-26 16:25:35 -0400

    product class done, product controller kinda done

---
+++ Csharp2SQLSolution/Csharp2SQLLibrary/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp2SQLLibrary
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int VendorId { get; set; }
+        public string Name { get; set; }
+        public string PartNbr { get; set; }
+        public decimal Price { get; set; }
+        public string Unit { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
---
+++ Csharp2SQLSolution/Csharp2SQLLibrary/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp2SQLLibrary
+{
+    public class ProductController
+    {
+        private static Connection connection { get; set; }
+
+
+        public List<Product> GetAllProducts()
+        {
+            var sql = $" SELECT * from Products; ";
+            var cmd = new SqlCommand(sql, connection.SqlConn);
+            var reader = cmd.ExecuteReader();
+            var products = new List<Product>();
+
+            while (reader.Read())
+            {
+                var product = new Product()
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    VendorId = Convert.ToInt32(reader["VendorId"]),
+                    PartNbr = Convert.ToString(reader["PartNbr"]),
+                    Name = Convert.ToString(reader["Name"]),
+                    Price = Convert.ToDecimal(reader["Price"]),
+                    Unit = Convert.ToString(reader["Unit"]),
+                    PhotoPath = Convert.ToString(reader["PhotoPath"])
+                };
+                products.Add(product);
+            }
+            reader.Close();
+            return products;
+        }
+    }
+}
