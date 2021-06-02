e7ed131204206f3593f41ef00a84de696015d738
Charlie Traylor <ctraylor@tql.com>
2021-06-02 16:22:22 -0400

    employee table and controller complete

---
+++ PoWebAPI/PoWebAPI/Data/PoContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PoWebAPI.Models;
+
+namespace PoWebAPI.Data
+{
+    public class PoContext : DbContext
+    {
+        public PoContext (DbContextOptions<PoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Employee>(e => { e.HasIndex(p => p.Login).IsUnique(); });
+        }
+    }
+}
